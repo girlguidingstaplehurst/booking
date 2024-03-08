832a96bbbe5db36ce78426463a791467f0c989fc
David Edmonds <edmonds.d.r@gmail.com>
2024-03-08 09:58:55 +0000

    feat: begin implementing list bookings

--- internal/calendar/handler.go
+++ internal/calendar/handler.go
@@ -4,21 +4,21 @@ import (
 	"context"
 
 	"connectrpc.com/connect"
-	v1 "github.com/girlguidingstaplehurst/booking/pkg/pb/v1"
-	calendarv1connect "github.com/girlguidingstaplehurst/booking/pkg/pb/v1/v1connect"
+	v1 "github.com/girlguidingstaplehurst/booking/pkg/api/v1"
+	calendarv1connect "github.com/girlguidingstaplehurst/booking/pkg/api/v1/v1connect"
 )
 
-var _ calendarv1connect.GreetServiceHandler = (*Handler)(nil)
+var _ calendarv1connect.BookingsServiceHandler = (*Handler)(nil)
 
 type Handler struct {
-	calendarv1connect.UnimplementedGreetServiceHandler
+	calendarv1connect.UnimplementedBookingsServiceHandler
 }
 
 func NewHandler() *Handler {
 	return &Handler{}
 }
 
-// Greet implements v1connect.GreetServiceHandler.
-func (h *Handler) Greet(ctx context.Context, req *connect.Request[v1.GreetRequest]) (*connect.Response[v1.GreetResponse], error) {
+// ListBookings lists all bookings between the specified dates. If not set, defaults to the bookings for the current month.
+func (h *Handler) ListBookings(ctx context.Context, req *connect.Request[v1.ListBookingsRequest]) (*connect.Response[v1.ListBookingsResponse], error) {
 	panic("unimplemented")
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -2,28 +2,31 @@ package service
 
 import (
 	"context"
+	"log/slog"
 	"net/http"
 
 	"github.com/girlguidingstaplehurst/booking/internal/calendar"
-	calendarv1connect "github.com/girlguidingstaplehurst/booking/pkg/pb/v1/v1connect"
+	calendarv1connect "github.com/girlguidingstaplehurst/booking/pkg/api/v1/v1connect"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 )
 
 type Service struct {
-	greeter calendarv1connect.GreetServiceHandler
+	bookings calendarv1connect.BookingsServiceHandler
 }
 
 func NewService() *Service {
 	return &Service{
-		greeter: calendar.NewHandler(),
+		bookings: calendar.NewHandler(),
 	}
 }
 
 func (s *Service) Run(ctx context.Context) error {
 	mux := http.NewServeMux()
-	path, handler := calendarv1connect.NewGreetServiceHandler(s.greeter)
+	path, handler := calendarv1connect.NewBookingsServiceHandler(s.bookings)
 	mux.Handle(path, handler)
+
+	slog.Info("booting grpc service")
 	http.ListenAndServe(
 		"localhost:8080",
 		// Use h2c so we can serve HTTP/2 without TLS.
